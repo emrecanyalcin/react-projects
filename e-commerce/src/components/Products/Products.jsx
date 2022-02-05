540efa9547ab6934e20f5fbe6844b1d17586d667
Emre Can <emrecanyalcin1996@gmail.com>
2022-02-05 14:59:42 +0300

    feat: Added Navbar component

--- e-commerce/src/components/Products/Products.jsx
+++ e-commerce/src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
   { id: 1, name: 'Shoes' , description: 'Running shoes', price: '$5', image: 'https://cdn-ss.akinon.net/products/2021/01/26/143326/fff4112f-12a8-47b3-8e1b-721467ab46f2.jpg', },
@@ -9,8 +10,11 @@ const products = [
 ]
 
 const Products = () => {
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div  className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>

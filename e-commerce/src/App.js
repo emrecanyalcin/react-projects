e15d28536dba123d0493f866538b2ddb7ab81329
Emre Can <emrecanyalcin1996@gmail.com>
2022-02-06 01:19:31 +0300

    feat: Added commerce products, add to cart, cart layout, and react router.

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,16 +1,45 @@
-import React from 'react'
-// import Products from './components/Products/Products';
-// import Navbar from './.components/Navbar/Navbar';
-
-import { Products, Navbar } from './components/'
+import React, { useState, useEffect } from 'react'
+import { commerce } from './lib/commerce';
+import { Products, Navbar, Cart } from './components/';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
+  const [ products, setProduct ] = useState([]);
+  const [ cart, setCart ] = useState({ line_items: [] });
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProduct(data);
+  }
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve())
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  console.log(products);
+  console.log(cart);
   return (
-    <div>
-      <Navbar />
-      <Products />
-      
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Routes>
+          <Route exact path="/" element={<Products products={products} onAddToCart={handleAddToCart} />} />
+          <Route exact path="/cart" element={<Cart card={cart} />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
--- e-commerce/src/components/Products/Products.jsx
+++ e-commerce/src/components/Products/Products.jsx
@@ -4,21 +4,16 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-  { id: 1, name: 'Shoes' , description: 'Running shoes', price: '$5', image: 'https://cdn-ss.akinon.net/products/2021/01/26/143326/fff4112f-12a8-47b3-8e1b-721467ab46f2.jpg', },
-  { id: 2, name: 'Macbook' , description: 'Apple Macbook', price: '$45', image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp14-spacegray-select-202110_GEO_TR?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1633657407000', },
-]
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
 
   return (
     <main className={classes.content}>
       <div  className={classes.toolbar} />
-      <Grid container justify="center" spacing={4}>
+      <Grid container justifyContent="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
